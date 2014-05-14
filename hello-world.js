80463d3b22d521886c462b23377dbb5a8bc9282a
Jer Thorp <blprnt@blprnt.com>
2014-05-14 15:14:36 -0400

    Figuring out .js includes.

--- hello-world.js
+++ hello-world.js
@@ -2,10 +2,9 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World!! Exclamation!" + tryThis());
+  var t = require('wordplay.js').tryThis();
+  response.write("Hello World!! Exclamation!" + t;
   response.end();
 }).listen(12892);
 
-function tryThis() {
-	return("BLABBBERSCYTHE!");
-}
+
