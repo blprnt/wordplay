3f0b96ae51bde29fea42113f40d26a58c5a16658
Jer Thorp <blprnt@blprnt.com>
2014-05-14 15:11:20 -0400

    Figuring this node thing out.

--- hello-world.js
+++ hello-world.js
@@ -2,6 +2,10 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World!! Exclamation!");
+  response.write("Hello World!! Exclamation!" + tryThis());
   response.end();
 }).listen(12892);
+
+function tryThis() {
+	return("BLABBBERSCYTHE!");
+}
