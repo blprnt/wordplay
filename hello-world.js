28e0a9b1a1bbaa1f4c5d0253cffa13a452b466f6
Jer Thorp <blprnt@blprnt.com>
2014-05-14 15:15:33 -0400

    Typo.

--- hello-world.js
+++ hello-world.js
@@ -3,7 +3,7 @@ var http = require("http");
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   var t = require('wordplay.js').tryThis();
-  response.write("Hello World!! Exclamation!" + t;
+  response.write("Hello World!! Exclamation!" + t);
   response.end();
 }).listen(12892);
 
