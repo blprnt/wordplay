b2c7d53046cc715a7037c7679c0c63c52d6a40b7
Jer Thorp <blprnt@blprnt.com>
2014-05-14 14:59:50 -0400

    Changing remote hello world.

--- hello-world.js
+++ hello-world.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
+  response.write("Hello World!! Exclamation!");
   response.end();
 }).listen(12892);
