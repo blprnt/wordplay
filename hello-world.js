7d0b764c756b752ea6e3f909bcb5971f7b84ec34
Jer Thorp <blprnt@blprnt.com>
2014-05-15 10:56:31 -0400

    Lots of changes. Big imporvements to text search, templating system installed.

--- hello-world.js
+++ hello-world.js
@@ -1,31 +1,60 @@
-var express = require('express');
+/*
+
+WordPlay Server (node.js)
+The Office For Creative Research
+May, 2014
+
+https://github.com/blprnt/wordplay
+
+*/
+
+var express = require('express')
+require('express')
+  , stylus = require('stylus')
+  , nib = require('nib');
+
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
+//CONFIGURE JADE, NIB, STYLUS
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
 
-app.get('/:corpus/:input', function(req, res){
+//PAGE SERVING
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+})
+
+//API CALLS
+app.get('wp/:corpus/:input/:max', function(req, res){
   //res.send('Process ' + req.params.input + ' from ' + req.params.corpus);
-  res.send(JSON.stringify(require('wordplay.js').getWordMatches(req.params.input, req.params.corpus)), null, 4);
+  res.send(JSON.stringify(require('wordplay.js').getWordMatches(req.params.input, req.params.corpus, req.params.max)), null, 4);
 });
 
+app.get('wp/:corpus/:input', function(req, res){
+  //res.send('Process ' + req.params.input + ' from ' + req.params.corpus);
+  res.send(JSON.stringify(require('wordplay.js').getWordMatches(req.params.input, req.params.corpus, 20)), null, 4);
+});
+
+//START IT UP.
 var server = app.listen(12892, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
 
-/*
-//This works.
-
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  var t = require('wordplay.js').tryThis();
-  response.write("Hello World!! Exclamation!" + t);
-  response.end();
-}).listen(12892);
-*/
 
 
